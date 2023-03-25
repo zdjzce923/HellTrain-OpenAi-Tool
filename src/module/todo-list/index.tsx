1ad8581eb7eb00775d1fa7b34bd9cca47cc4a4dd
zdjce923 <493904954@qq.com>
2023-03-25 14:50:20 +0800

    feat: todo list demo

--- src/module/todo-list/index.tsx
+++ src/module/todo-list/index.tsx
@@ -1,12 +1,25 @@
-
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, toRefs } from 'vue'
 
 const TodoList = defineComponent({
   name: 'TodoList',
-  setup() {},
+  setup() {
+    const data = reactive({
+      todoList: [1, 2, 3, 4, 5],
+      doneList: [],
+    })
+
+    return {
+      ...toRefs(data),
+    }
+  },
   render() {
-    return <div>kasdjkasjkasjkd</div>
-  }
+    console.log('this.todoList', this.todoList)
+    return(
+      this.todoList.map(item => {
+        return <div class="px-[10px] py-[5px] max-w-[30px] text-[blue] my-[20px] bg-slate-400">{item}</div>
+      })
+    )
+  },
 })
 
 export default TodoList
